9c2a1fc4bead47f7a5d4a0ace97ae95b1a7c42ec
KaterynaHolt <kholtvianskatech@gmail.com>
2022-11-02 20:56:40 +0200

    SCA-3 Implemented functionality to pass and display elements in alphabetical order

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
     public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println("Please enter your words with spaces between them:");
+        Scanner scanner = new Scanner(System.in);
+        String[] str = scanner.nextLine().split(" ");
+        Stream<String> stream = Arrays.stream(str);
+        System.out.println("Sorted word list: ");
+        stream.sorted().forEach(x -> System.out.println(x));
+        stream.close();
+        scanner.close();
     }
 }
