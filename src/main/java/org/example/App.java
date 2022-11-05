53e8779ed0ef1466f88836848a5ddbb75c91bc2a
KaterynaHolt <kholtvianskatech@gmail.com>
2022-11-05 18:40:47 +0200

    SCA-3 Edited method main, created special method for sorting and created unit test fot it

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,15 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.stream.Stream;
 
-    public class App {
+public class App {
     public static void main( String[] args ) {
-        System.out.println("Please enter your words with spaces between them:");
-        Scanner scanner = new Scanner(System.in);
-        String[] str = scanner.nextLine().split(" ");
-        Stream<String> stream = Arrays.stream(str);
         System.out.println("Sorted word list: ");
-        stream.sorted().forEach(x -> System.out.println(x));
-        stream.close();
-        scanner.close();
+        Arrays.stream(sorted_list(args)).forEach(x -> System.out.println(x));
+    }
+
+    public static Object[] sorted_list(String[] str){
+        return Arrays.stream(str).sorted().toArray();
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Unit test for simple App.
@@ -30,5 +37,18 @@ public class AppTest
      */
     public void testApp() {
         assertTrue( true );
+    }
+
+    public void testList(){
+        String[] data = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        String[] expected = {"eight", "five", "four", "nine", "one", "seven", "six", "ten", "three", "two"};
+        Object[] actual = App.sorted_list(data);
+
+        Iterator<String> ex = Arrays.stream(expected).iterator();
+        Iterator<Object> ac = Arrays.stream(actual).iterator();
+        while(ex.hasNext() && ac.hasNext()){
+            assertEquals(ex.next(), ac.next());
+        }
+        assert !ex.hasNext() && !ac.hasNext();
     }
 }
