dc44afc807eb648fa0d7cbe85822d5148589cba6
KaterynaHolt <kholtvianskatech@gmail.com>
2022-11-11 20:59:15 +0200

    SCA-4 Added validation of passed parameters

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,10 +3,10 @@ package org.example;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.lang.IllegalArgumentException;
 
 public class App {
-    public static void main( String[] args ) {
-        System.out.println("Sorted word list: ");
+    public static void main( String[] args ){
         sortList(args).forEach(x -> System.out.println(x));
     }
 
@@ -15,7 +15,16 @@ public class App {
      * @param str Array of words which must be sorted
      * @return List of sorted words
      */
-    public static List<String> sortList(String[] str){
-        return Arrays.asList(str).stream().sorted().collect(Collectors.toList());
+    public static List<String> sortList(String[] str) throws IllegalArgumentException{
+        if(str.length == 0){
+            throw new IllegalArgumentException("No parameter passed!");
+        }
+        else if(str.length == 1) {
+            return Arrays.asList("You passed only one parameter, it must be two or more!");
+        }
+        else{
+            System.out.println("Sorted word list: ");
+            return Arrays.asList(str).stream().sorted().collect(Collectors.toList());
+        }
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,11 +4,10 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Unit test for simple App.
@@ -46,6 +45,33 @@ public class AppTest
         String[] data = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
         List<String> expected = List
                 .of("eight", "five", "four", "nine", "one", "seven", "six", "ten", "three", "two");
+        //WHEN
+        List<String> actual = App.sortList(data);
+        //THEN
+        Assertions.assertLinesMatch(expected, actual);
+    }
+
+    /**
+     * Test throwing IllegalArgumentException in method sortedList
+     */
+    public void test_no_parameter_exception(){
+        //GIVEN
+        String[] data = {};
+        //WHEN
+        IllegalArgumentException throwable = Assertions.assertThrows(IllegalArgumentException
+                .class, ()-> App.sortList(data));
+        //THEN
+        Assertions.assertEquals("No parameter passed!", throwable.getMessage());
+    }
+
+    /**
+     * Test message that method sortedList was passed only one parameter
+     */
+    public void test_one_element_exception(){
+        //GIVEN
+        String[] data = {"one"};
+        List<String> expected = List
+                .of("You passed only one parameter, it must be two or more!");
         //WHEN
         List<String> actual = App.sortList(data);
         //THEN
